25e99dd462a76066804cc4eb5029552fb24fc66a
Valentin DUFLOT <duflot.valentin@gmail.com>
2022-08-22 16:03:00 +0200

    <Feat> Premier commit avec convertisseur CSV->SQL fonctionnel

---
+++ src/main/java/fr/jpa/Convertisseur.java
@@ -0,0 +1,114 @@
+package fr.jpa;
+
+import java.io.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class Convertisseur {
+	
+	private static EntityManagerFactory sessionFactory; 
+	private static EntityManager manager;
+	
+	public static void setUp() {
+		sessionFactory = Persistence.createEntityManagerFactory("my-persistence-unit"); 
+		Convertisseur.manager = sessionFactory.createEntityManager();
+	}
+	public static EntityManager getManager()  { return manager; } 
+	
+	public static void main(String[] args) {
+		
+		setUp();
+		
+		try {
+			File file = new File("base.csv");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String ligne;
+			while((ligne=bufferedReader.readLine())!=null) {
+				
+				stringBuffer.append(ligne);
+
+
+				creerJeu(stringBuffer.toString());
+				stringBuffer.delete(0, stringBuffer.length());
+			}
+			fileReader.close();
+		}
+		catch(IOException exception) {  
+			exception.printStackTrace();  
+		}  
+	}
+	
+	public static void creerJeu(String contenu) {
+		
+		String[] liste = new String[15];
+		
+		String[] splitageGuillemet = contenu.split("\"");
+		if(splitageGuillemet.length == 1)
+		{
+			String[] splitage = contenu.split(",");
+			for(int i = 0; i < 15; i++) {
+	        	
+	        	if(splitage.length <= i) {
+	        		liste[i] = "0";
+	        	}
+	        	else {
+	        		liste[i] = splitage[i];
+	        	}
+	        	
+	        }
+		}
+		else {
+			liste[0] = splitageGuillemet[0];
+			String[] splitage = splitageGuillemet[1].split(",");
+			for(int i = 1; i < 15; i++) {
+	        	
+	        	if(splitage.length <= i) {
+	        		liste[i] = "0";
+	        	}
+	        	else {
+	        		liste[i] = splitage[i];
+	        	}
+	        	
+	        }
+		}
+        		
+        String nom = liste[0];
+    	String plateforme = liste[1];
+    	int anneeDeSortie = Integer.parseInt(liste[2]);
+    	String genre = liste[3];
+    	String editeur = liste[4];
+    	double ventesNA = Double.parseDouble(liste[5]);
+    	double ventesEU = Double.parseDouble(liste[6]);
+    	double ventesJP = Double.parseDouble(liste[7]);
+    	double ventesAutre = Double.parseDouble(liste[8]);
+    	double ventesGlobal = Double.parseDouble(liste[9]);
+    	float critiqueMoyenne = Float.parseFloat(liste[10]);
+    	double nombreCritiques = Double.parseDouble(liste[11]);
+    	float notesJoueurs = Float.parseFloat(liste[12]);
+    	double nombreNotesJoueurs = Double.parseDouble(liste[13]);
+    	String rating = liste[14];
+    	
+    	
+    	Jeu jeu = new Jeu(nom,plateforme,anneeDeSortie,genre,editeur,ventesNA,ventesEU,ventesJP,ventesAutre,ventesGlobal,critiqueMoyenne,nombreCritiques,
+    			notesJoueurs,nombreNotesJoueurs,rating);
+    	
+    	manager.getTransaction().begin();
+		manager.persist(jeu);
+		manager.getTransaction().commit();
+    	
+    			
+		
+	}
+	
+	
+	
+	
+	
+
+
+}
---
+++ src/main/java/fr/jpa/Jeu.java
@@ -0,0 +1,271 @@
+package fr.jpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Jeu {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String nom;
+	
+	@Column(nullable = false)
+	private String plateforme;
+	
+	private int anneeDeSortie;
+	
+	@Column(nullable = false)
+	private String genre;
+	
+	@Column(nullable = false)
+	private String editeur;
+	
+	private double ventesNA;
+	private double ventesEU;
+	private double ventesJP;
+	private double ventesAutre;
+	private double ventesGlobal;
+	private float critiqueMoyenne;
+	private double nombreCritiques;
+	private float notesJoueurs;
+	private double nombreNotesJoueurs;
+	
+	@Column(nullable = false)
+	private String rating;
+	
+	
+	
+	public Jeu(String nom, String plateforme, int anneeDeSortie, String genre, String editeur, double ventesNA,
+			double ventesEU, double ventesJP, double ventesAutre, double ventesGlobal, float critiqueMoyenne,
+			double nombreCritiques, float notesJoueurs, double nombreNotesJoueurs, String rating) {
+		super();
+		this.nom = nom;
+		this.plateforme = plateforme;
+		this.anneeDeSortie = anneeDeSortie;
+		this.genre = genre;
+		this.editeur = editeur;
+		this.ventesNA = ventesNA;
+		this.ventesEU = ventesEU;
+		this.ventesJP = ventesJP;
+		this.ventesAutre = ventesAutre;
+		this.ventesGlobal = ventesGlobal;
+		this.critiqueMoyenne = critiqueMoyenne;
+		this.nombreCritiques = nombreCritiques;
+		this.notesJoueurs = notesJoueurs;
+		this.nombreNotesJoueurs = nombreNotesJoueurs;
+		this.rating = rating;
+	
+	}
+	
+	/**
+	 * Constructeur vide nécessaire pour JPA
+	 */
+	public Jeu() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the plateforme
+	 */
+	public String getPlateforme() {
+		return plateforme;
+	}
+	/**
+	 * @param plateforme the plateforme to set
+	 */
+	public void setPlateforme(String plateforme) {
+		this.plateforme = plateforme;
+	}
+	/**
+	 * @return the anneeDeSortie
+	 */
+	public int getAnneeDeSortie() {
+		return anneeDeSortie;
+	}
+	/**
+	 * @param anneeDeSortie the anneeDeSortie to set
+	 */
+	public void setAnneeDeSortie(int anneeDeSortie) {
+		this.anneeDeSortie = anneeDeSortie;
+	}
+	/**
+	 * @return the genre
+	 */
+	public String getGenre() {
+		return genre;
+	}
+	/**
+	 * @param genre the genre to set
+	 */
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	/**
+	 * @return the editeur
+	 */
+	public String getEditeur() {
+		return editeur;
+	}
+	/**
+	 * @param editeur the editeur to set
+	 */
+	public void setEditeur(String editeur) {
+		this.editeur = editeur;
+	}
+	/**
+	 * @return the ventesNA
+	 */
+	public double getVentesNA() {
+		return ventesNA;
+	}
+	/**
+	 * @param ventesNA the ventesNA to set
+	 */
+	public void setVentesNA(double ventesNA) {
+		this.ventesNA = ventesNA;
+	}
+	/**
+	 * @return the ventesEU
+	 */
+	public double getVentesEU() {
+		return ventesEU;
+	}
+	/**
+	 * @param ventesEU the ventesEU to set
+	 */
+	public void setVentesEU(double ventesEU) {
+		this.ventesEU = ventesEU;
+	}
+	/**
+	 * @return the ventesJP
+	 */
+	public double getVentesJP() {
+		return ventesJP;
+	}
+	/**
+	 * @param ventesJP the ventesJP to set
+	 */
+	public void setVentesJP(double ventesJP) {
+		this.ventesJP = ventesJP;
+	}
+	/**
+	 * @return the ventesAutre
+	 */
+	public double getVentesAutre() {
+		return ventesAutre;
+	}
+	/**
+	 * @param ventesAutre the ventesAutre to set
+	 */
+	public void setVentesAutre(double ventesAutre) {
+		this.ventesAutre = ventesAutre;
+	}
+	/**
+	 * @return the ventesGlobal
+	 */
+	public double getVentesGlobal() {
+		return ventesGlobal;
+	}
+	/**
+	 * @param ventesGlobal the ventesGlobal to set
+	 */
+	public void setVentesGlobal(double ventesGlobal) {
+		this.ventesGlobal = ventesGlobal;
+	}
+	/**
+	 * @return the critiqueMoyenne
+	 */
+	public float getCritiqueMoyenne() {
+		return critiqueMoyenne;
+	}
+	/**
+	 * @param critiqueMoyenne the critiqueMoyenne to set
+	 */
+	public void setCritiqueMoyenne(float critiqueMoyenne) {
+		this.critiqueMoyenne = critiqueMoyenne;
+	}
+	/**
+	 * @return the nombreCritiques
+	 */
+	public double getNombreCritiques() {
+		return nombreCritiques;
+	}
+	/**
+	 * @param nombreCritiques the nombreCritiques to set
+	 */
+	public void setNombreCritiques(double nombreCritiques) {
+		this.nombreCritiques = nombreCritiques;
+	}
+	/**
+	 * @return the notesJoueurs
+	 */
+	public float getNotesJoueurs() {
+		return notesJoueurs;
+	}
+	/**
+	 * @param notesJoueurs the notesJoueurs to set
+	 */
+	public void setNotesJoueurs(float notesJoueurs) {
+		this.notesJoueurs = notesJoueurs;
+	}
+	/**
+	 * @return the nombreNotesJoueurs
+	 */
+	public double getNombreNotesJoueurs() {
+		return nombreNotesJoueurs;
+	}
+	/**
+	 * @param nombreNotesJoueurs the nombreNotesJoueurs to set
+	 */
+	public void setNombreNotesJoueurs(double nombreNotesJoueurs) {
+		this.nombreNotesJoueurs = nombreNotesJoueurs;
+	}
+	/**
+	 * @return the rating
+	 */
+	public String getRating() {
+		return rating;
+	}
+	/**
+	 * @param rating the rating to set
+	 */
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	
+	
+	
+}
