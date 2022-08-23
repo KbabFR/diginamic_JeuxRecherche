48e13db6f48113697caec9d2491759b0221c122f
Valentin DUFLOT <duflot.valentin@gmail.com>
2022-08-23 11:15:58 +0200

    <Feat> Ajout des classes initiales User et Avis

--- src/main/java/fr/jpa/model/Avis.java
+++ src/main/java/fr/jpa/model/Avis.java
@@ -1,5 +1,109 @@
 package fr.jpa.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Avis {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private long id;
+	
+	private User user;
+	private Jeu jeu;
+	
+	private String contenu;
+	
+	@Column(nullable = false)
+	private float note;
+	
+	public Avis() {
+		
+	}
+
+	public Avis(long id, User user, Jeu jeu, String contenu, float note) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.jeu = jeu;
+		this.contenu = contenu;
+		this.note = note;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the jeu
+	 */
+	public Jeu getJeu() {
+		return jeu;
+	}
+
+	/**
+	 * @param jeu the jeu to set
+	 */
+	public void setJeu(Jeu jeu) {
+		this.jeu = jeu;
+	}
+
+	/**
+	 * @return the contenu
+	 */
+	public String getContenu() {
+		return contenu;
+	}
+
+	/**
+	 * @param contenu the contenu to set
+	 */
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	/**
+	 * @return the note
+	 */
+	public float getNote() {
+		return note;
+	}
+
+	/**
+	 * @param note the note to set
+	 */
+	public void setNote(float note) {
+		this.note = note;
+	}
+	
+	
 
 }
--- src/main/java/fr/jpa/model/User.java
+++ src/main/java/fr/jpa/model/User.java
@@ -1,5 +1,133 @@
 package fr.jpa.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private long id;
+	
+	@Column(nullable = false)
+	private String prenom;
+
+	@Column(nullable = false)
+	private String nom;
+	
+
+	@Column(nullable = false)
+	private String mail;
+	
+
+	@Column(nullable = false)
+	private String pass;
+	
+	private List<Child> listeEnfants;
+	//TODO la méthode d'ajout d'enfant à cette liste
+	
+	public User() {
+		
+	}
+	
+	public User(long id, String prenom, String nom, String mail, String pass) {
+		this.id = id;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.mail = mail;
+		this.pass = pass;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the mail
+	 */
+	public String getMail() {
+		return mail;
+	}
+
+	/**
+	 * @param mail the mail to set
+	 */
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	/**
+	 * @return the pass
+	 */
+	public String getPass() {
+		return pass;
+	}
+
+	/**
+	 * @param pass the pass to set
+	 */
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	/**
+	 * @return the listeEnfants
+	 */
+	public List<Child> getListeEnfants() {
+		return listeEnfants;
+	}
+
+	/**
+	 * @param listeEnfants the listeEnfants to set
+	 */
+	public void setListeEnfants(List<Child> listeEnfants) {
+		this.listeEnfants = listeEnfants;
+	}
 
 }
