184166779dcf5990fb6f24fb333378d4d2cc8927
Justine Brun <brun.justine@outlook.fr>
2022-09-02 13:46:50 +0200

    popupdonnerunavis

--- src/app/components/card-avis/card-avis.component.ts
+++ src/app/components/card-avis/card-avis.component.ts
@@ -12,4 +12,5 @@ export class CardAvisComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }
--- src/app/components/card-resultat/card-resultat.component.ts
+++ src/app/components/card-resultat/card-resultat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-card-resultat',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-resultat.component.css']
 })
 export class CardResultatComponent implements OnInit {
+  loginForm : FormGroup = this.fb.group({  //Crée une instance de FormGroup
+    username: [],                   // Crée une instance de FormControl
+    password: [],                   // Crée une instance de FormControl
+  });
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {}
 
   ngOnInit(): void {
+  }
+  displayStyle = "none";
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
   }
 
 }
