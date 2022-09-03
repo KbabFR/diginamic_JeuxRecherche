25dbbbd8799a9070abff0ede84b9ed86b31cdbb8
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-09-03 18:48:39 +0200

    crossOrigin

--- jeuxrecherche/src/main/java/fr/diginamic/jeuxrecherche/controllers/ChildController.java
+++ jeuxrecherche/src/main/java/fr/diginamic/jeuxrecherche/controllers/ChildController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.diginamic.jeuxrecherche.models.Child;
 import fr.diginamic.jeuxrecherche.services.ChildService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/rest/child")
 public class ChildController {
